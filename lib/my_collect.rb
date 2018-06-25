9cffe45efa9447d17d92415d230e0f238908b648
Justine Bricknell <justinegayton@gmail.com>
2018-06-25 14:59:37 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,21 +3,9 @@ def my_collect(array)
 collection = []
   while i < array.length
     collection << yeild(array[i]
-    i += 1
+    i+=1
   end
 collection
 end
 
 my_collect(array)
-
-
-
-
-
-
-
-
-array = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(array) do |name|
-  name.upcase
-end
