339669ee154830f1d632e9c1b0b31e2b12f4cedc
Justine Bricknell <justinegayton@gmail.com>
2018-06-25 14:58:02 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,22 @@
+def dogs(array)
+  i = 0
+  dogs = []
+  while i < array.length
+    dogs << yeild(array[i]
+    i += 1
+  end
+collection
+end
+
+
+
+
+
+
+
+
+
 array = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(array) do |name|
   name.upcase
 end
-
-# => ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
